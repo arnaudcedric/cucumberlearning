16480612ad69306a5abbd84abc788992b29e1752
acedric <cedricarno@gmail.com>
2017-09-29 00:12:25 +0100

    update

--- src/test/java/paypalRestAPI/BaseClassPaypal.java
+++ src/test/java/paypalRestAPI/BaseClassPaypal.java
@@ -11,7 +11,7 @@ public class BaseClassPaypal {
 
     public static String accessToken;
 
-    //PUT YOUR CLIENT ID & CLIENT SECRET HERE
+    //PUT YOUR CLIENT ID & CLIENT SECRET  HERE
     private final static String CLIENTID="ATz55n98p1ev9cstg7tf2uZZaOiEfbhbIT2__aifStGoKl3GKENACd7yGVK6QzNXry2sPM3oiSQOhXFQ";
     private final static String CLIENTSECRET="EGDnZyNzSZyXnhZ5AJVRHva_F0KhtNwE5DTgZfcO3Cgv0EBn1wU6ns5JLw4slRUPuRkX0ZdKtqwukjhq";
 
